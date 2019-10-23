8291b4e7f3a5b74d61c2f5ddcda4b7cba4e7c51d
TakahashiMasaya <ma.taka.ic@icloud.com>
2019-10-23 15:17:51 +0900

    add text

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
-/**
- * main.js
- */
-
 const DATA = [
-  { name: 'hoge1', age: 30, addr: 'Fukuoka' },
-  { name: 'hoge2', age: 40, addr: 'Osaka' },
-  { name: 'hoge3', age: 50, addr: 'Nagoya' },
-  { name: 'hoge4', age: 20, addr: 'Tokyo' },
-];
-
+    { name: 'hoge1', age: 30, addr: 'Fukuoka' },
+    { name: 'hoge2', age: 40, addr: 'Osaka' },
+    { name: 'hoge3', age: 50, addr: 'Nagoya' },
+    { name: 'hoge4', age: 20, addr: 'Tokyo' },
+]
 const htmlStr = `<div class="table">${DATA.map(_data => `<div class="rows"><div class="col">${_data.name}</div><div class="col">${_data.age}</div><div class="col">${_data.addr}</div></div>`).join('')}</div>`;
+
+test
+
 document.getElementsByTagName('div')[0].insertAdjacentHTML('afterbegin', htmlStr);
